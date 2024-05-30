442810f87ea9bbc2a47dd1edbf01e7b468716d5c
Ableboy <ebubemoses235@gmail.com>
2024-05-30 10:14:36 +0100

    removed hashtag

--- pdf.py
+++ pdf.py
@@ -20,35 +20,35 @@ with open("watermarked_output.pdf", "wb") as file: # new file that holds the wat
 
 
 
-# # ALSO: Check out the below code and remove the commit up beside import sys 
+# ALSO: Check out the below code and remove the commit up beside import sys 
 
-# '''This is to combine multiple pdf files as one file'''
-# input = sys.argv[1:] # keep all argv from second value
+'''This is to combine multiple pdf files as one file'''
+input = sys.argv[1:] # keep all argv from second value
 
-# def combiner(pdf_list): # The func combines pdf files using merger
-#     merger = PyPDF2.PdfMerger() # merger obj
-#     for pdf in pdf_list: # literate over pdf file
-#         print(pdf)
-#         merger.append(pdf) # This saves and keeps file in new py file
-#     merger.write('super.pdf') # new py file creation without using with func to write or read file.
+def combiner(pdf_list): # The func combines pdf files using merger
+    merger = PyPDF2.PdfMerger() # merger obj
+    for pdf in pdf_list: # literate over pdf file
+        print(pdf)
+        merger.append(pdf) # This saves and keeps file in new py file
+    merger.write('super.pdf') # new py file creation without using with func to write or read file.
 
-# combiner(input)
+combiner(input)
 
 
-# # Continue exploring
+# Continue exploring
 
-# '''This is to rotate the pdf file'''
-# # Test to understand pdf code file
+'''This is to rotate the pdf file'''
+# Test to understand pdf code file
 
-# with open("dummy.pdf", "rb") as file: # open a file and make it accessable by using rb= read binary
-#     reader = PyPDF2.PdfReader(file) # This read the pdf file
-#     num_pages = len(reader.pages) # This shows the num of pdf file
-#     writer = PyPDF2.PdfWriter() # This over write the file and trying changing to rotating form
-#     for page in range(num_pages): # loop into the file and rotate 
-#         page_obj = reader.pages[page]
-#         page_obj.rotate(90)  # Rotate the page 90 degrees clockwise
-#         writer.add_page(page_obj)
+with open("dummy.pdf", "rb") as file: # open a file and make it accessable by using rb= read binary
+    reader = PyPDF2.PdfReader(file) # This read the pdf file
+    num_pages = len(reader.pages) # This shows the num of pdf file
+    writer = PyPDF2.PdfWriter() # This over write the file and trying changing to rotating form
+    for page in range(num_pages): # loop into the file and rotate 
+        page_obj = reader.pages[page]
+        page_obj.rotate(90)  # Rotate the page 90 degrees clockwise
+        writer.add_page(page_obj)
 
-#     with open("tilt.pdf", "wb") as new_file: # Turn the rotated file into another file by over riding it using wb= write binary
-#         writer.write(new_file)
+    with open("tilt.pdf", "wb") as new_file: # Turn the rotated file into another file by over riding it using wb= write binary
+        writer.write(new_file)
 
